c7c34b40a19c90076745e1322478f22297a29ba6
joona95 <joona95@naver.com>
2023-11-07 00:33:29 +0900

    feat: 관람 등급 enum 추가

---
+++ src/main/java/com/culture/ticketing/show/domain/AgeRestriction.java
@@ -0,0 +1,14 @@
+package com.culture.ticketing.show.domain;
+
+public enum AgeRestriction {
+    ALL("전체관람가"),
+    TWELVE("12세 이상 관람가"),
+    FIFTEEN("15세 이상 관람가"),
+    ADULT("청소년 관람불가");
+
+    private String ageRestrictionName;
+
+    AgeRestriction(String ageRestrictionName) {
+        this.ageRestrictionName = ageRestrictionName;
+    }
+}
