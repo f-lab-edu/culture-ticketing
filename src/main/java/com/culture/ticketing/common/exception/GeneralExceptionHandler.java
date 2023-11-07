e147da7e72395efbd51a4fa2d5d34be2cfc874cc
joona95 <joona95@naver.com>
2023-11-07 16:46:51 +0900

    feat: validation 메세지 응답 예외 핸들러 추가

--- src/main/java/com/culture/ticketing/common/exception/GeneralExceptionHandler.java
+++ src/main/java/com/culture/ticketing/common/exception/GeneralExceptionHandler.java
@@ -2,16 +2,16 @@ package com.culture.ticketing.common.exception;
 
 import com.culture.ticketing.common.response.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLException;
 
 import static com.culture.ticketing.common.response.BaseResponse.error;
-import static com.culture.ticketing.common.response.BaseResponseStatus.DATABASE_ERROR;
-import static com.culture.ticketing.common.response.BaseResponseStatus.UNKNOWN_ERROR;
+import static com.culture.ticketing.common.response.BaseResponseStatus.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GeneralExceptionHandler {
 
@@ -19,6 +19,12 @@ public class GeneralExceptionHandler {
     public BaseResponse<?> handleBaseException(BaseException e) {
         log.error(e.getClass().getName(), e);
         return error(e);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public BaseResponse<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getClass().getName(), e);
+        return error(new BaseException(BAD_REQUEST), e.getBindingResult().getFieldErrors().get(0).getDefaultMessage());
     }
 
     @ExceptionHandler({SQLException.class})
--- src/main/java/com/culture/ticketing/common/response/BaseResponseStatus.java
+++ src/main/java/com/culture/ticketing/common/response/BaseResponseStatus.java
@@ -10,6 +10,8 @@ public enum BaseResponseStatus {
 
     SUCCESS(200, "요청에 성공하였습니다."),
 
+    BAD_REQUEST(400, "잘못된 요청값입니다."),
+
     DATABASE_ERROR(500, "데이터베이스 조회에 실패하였습니다."),
     UNKNOWN_ERROR(500, "알수 없는 에러가 발생하였습니다.");
 
