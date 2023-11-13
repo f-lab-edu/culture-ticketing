65711e3ee26b49cbfe7d154cceb43a06cb60c8d1
joona95 <joona95@naver.com>
2023-11-13 20:10:06 +0900

    fix: transactional 메서드 단위로 지정하도록 수정

--- src/main/java/com/culture/ticketing/place/application/PlaceService.java
+++ src/main/java/com/culture/ticketing/place/application/PlaceService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 public class PlaceService {
 
     private final PlaceRepository placeRepository;
@@ -28,6 +27,7 @@ public class PlaceService {
         placeRepository.save(place);
     }
 
+    @Transactional(readOnly = true)
     public Place getPlaceByPlaceId(Long placeId) {
         return placeRepository.findById(placeId).orElseThrow();
     }
