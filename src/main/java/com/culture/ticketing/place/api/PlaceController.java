334dd7647168f834ce49bb739c4c5f4d9efc7ba4
joona95 <joona95@naver.com>
2023-11-13 20:33:21 +0900

    fix: response 응닶값 추가 구조 제거

--- src/main/java/com/culture/ticketing/place/api/PlaceController.java
+++ src/main/java/com/culture/ticketing/place/api/PlaceController.java
@@ -1,13 +1,10 @@
 package com.culture.ticketing.place.api;
 
-import com.culture.ticketing.common.response.BaseResponse;
 import com.culture.ticketing.place.application.PlaceService;
 import com.culture.ticketing.place.application.dto.PlaceSaveRequest;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
-import static com.culture.ticketing.common.response.BaseResponse.success;
 
 @RestController
 @RequestMapping("/places")
@@ -21,10 +18,8 @@ public class PlaceController {
 
     @ResponseBody
     @PostMapping("")
-    public BaseResponse<Void> postPlace(@Valid @RequestBody PlaceSaveRequest request) {
+    public void postPlace(@Valid @RequestBody PlaceSaveRequest request) {
 
         placeService.createPlace(request);
-
-        return success();
     }
 }
