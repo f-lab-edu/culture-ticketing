96011233e21560eceeff9b20bbacaabe1e864220
joona95 <joona95@naver.com>
2023-11-14 20:22:24 +0900

    fix: service 내부에서도 요청값 유효성 검사 추가

--- src/main/java/com/culture/ticketing/place/application/PlaceService.java
+++ src/main/java/com/culture/ticketing/place/application/PlaceService.java
@@ -1,10 +1,15 @@
 package com.culture.ticketing.place.application;
 
+import com.culture.ticketing.common.exception.BaseException;
+import com.culture.ticketing.common.response.BaseResponseStatus;
 import com.culture.ticketing.place.application.dto.PlaceSaveRequest;
 import com.culture.ticketing.place.domain.Place;
 import com.culture.ticketing.place.infra.PlaceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.math.BigDecimal;
 
 @Service
 public class PlaceService {
@@ -17,6 +22,16 @@ public class PlaceService {
 
     @Transactional
     public void createPlace(PlaceSaveRequest request) {
+
+        if (!StringUtils.hasText(request.getAddress())) {
+            throw new BaseException(BaseResponseStatus.EMPTY_PLACE_ADDRESS);
+        }
+        if (request.getLatitude() == null || request.getLatitude().compareTo(BigDecimal.ZERO) > 0) {
+            throw new BaseException(BaseResponseStatus.EMPTY_PLACE_LATITUDE);
+        }
+        if (request.getLongitude() == null || request.getLongitude().compareTo(BigDecimal.ZERO) > 0) {
+            throw new BaseException(BaseResponseStatus.EMPTY_PLACE_LONGITUDE);
+        }
 
         Place place = request.toEntity();
         placeRepository.save(place);
--- src/main/java/com/culture/ticketing/show/application/ShowService.java
+++ src/main/java/com/culture/ticketing/show/application/ShowService.java
@@ -1,5 +1,7 @@
 package com.culture.ticketing.show.application;
 
+import com.culture.ticketing.common.exception.BaseException;
+import com.culture.ticketing.common.response.BaseResponseStatus;
 import com.culture.ticketing.place.application.PlaceService;
 import com.culture.ticketing.place.domain.Place;
 import com.culture.ticketing.show.application.dto.ShowSaveRequest;
@@ -7,6 +9,7 @@ import com.culture.ticketing.show.domain.Show;
 import com.culture.ticketing.show.infra.ShowRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 @Service
 public class ShowService {
@@ -22,6 +25,31 @@ public class ShowService {
     @Transactional
     public void createShow(ShowSaveRequest request) {
 
+        if (request.getCategory() == null) {
+            throw new BaseException(BaseResponseStatus.EMPTY_SHOW_CATEGORY);
+        }
+        if (request.getAgeRestriction() == null) {
+            throw new BaseException(BaseResponseStatus.EMPTY_SHOW_AGE_RESTRICTION);
+        }
+        if (!StringUtils.hasText(request.getShowName())) {
+            throw new BaseException(BaseResponseStatus.EMPTY_SHOW_NAME);
+        }
+        if (request.getShowStartDate() == null) {
+            throw new BaseException(BaseResponseStatus.EMPTY_SHOW_START_DATE);
+        }
+        if (request.getShowEndDate() == null) {
+            throw new BaseException(BaseResponseStatus.EMPTY_SHOW_END_DATE);
+        }
+        if (request.getRunningTime() <= 0) {
+            throw new BaseException(BaseResponseStatus.NOT_POSITIVE_SHOW_RUNNING_TIME);
+        }
+        if (!StringUtils.hasText(request.getPosterImgUrl())) {
+            throw new BaseException(BaseResponseStatus.EMPTY_SHOW_POSTER_IMG_URL);
+        }
+        if (request.getPlaceId() == null) {
+            throw new BaseException(BaseResponseStatus.EMPTY_SHOW_PLACE_ID);
+        }
+        
         Place place = placeService.getPlaceByPlaceId(request.getPlaceId());
         Show show = request.toEntity(place);
         showRepository.save(show);
