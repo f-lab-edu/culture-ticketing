33376fc9181374ceed6256f6722ce14ec824d740
Joona Choi <juna052@naver.com>
2023-12-07 16:34:23 +0900

    fix: 장소 구역 생성 기능 요청값 조건 처리 수정

--- src/main/java/com/culture/ticketing/place/application/AreaService.java
+++ src/main/java/com/culture/ticketing/place/application/AreaService.java
@@ -1,13 +1,15 @@
 package com.culture.ticketing.place.application;
 
-import com.culture.ticketing.common.exception.BaseException;
-import com.culture.ticketing.common.response.BaseResponseStatus;
 import com.culture.ticketing.place.application.dto.PlaceAreaSaveRequest;
 import com.culture.ticketing.place.domain.Area;
-import com.culture.ticketing.place.exception.AreaNotFoundException;
+import com.culture.ticketing.place.exception.PlaceNotFoundException;
 import com.culture.ticketing.place.infra.AreaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+import static com.culture.ticketing.common.response.BaseResponseStatus.EMPTY_PLACE_ID;
 
 @Service
 public class AreaService {
@@ -28,11 +30,12 @@ public class AreaService {
     @Transactional
     public void createPlaceArea(PlaceAreaSaveRequest request) {
 
-        if (request.getPlaceId() == null) {
-            throw new BaseException(BaseResponseStatus.EMPTY_PLACE_ID);
+        Objects.requireNonNull(request.getPlaceId(), EMPTY_PLACE_ID.getMessage());
+
+        if (!placeService.existsById(request.getPlaceId())) {
+            throw new PlaceNotFoundException(request.getPlaceId());
         }
 
-        placeService.getPlaceByPlaceId(request.getPlaceId());
         Area area = request.toEntity();
         areaRepository.save(area);
     }
