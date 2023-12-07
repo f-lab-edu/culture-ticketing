0bdb3863a43a94a925bc8e0d2e793c625b13dcfb
Joona Choi <juna052@naver.com>
2023-12-07 17:03:18 +0900

    refactor: 공통 repository 구현 상속

--- src/main/java/com/culture/ticketing/show/infra/ShowRepositoryImpl.java
+++ src/main/java/com/culture/ticketing/show/infra/ShowRepositoryImpl.java
@@ -1,23 +1,19 @@
 package com.culture.ticketing.show.infra;
 
+import com.culture.ticketing.common.infra.BaseRepositoryImpl;
 import com.culture.ticketing.show.domain.Category;
 import com.culture.ticketing.show.domain.Show;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 import static com.culture.ticketing.show.domain.QShow.show;
 
-public class ShowRepositoryImpl implements ShowRepositoryCustom {
-
-    private final EntityManager em;
-    private final JPAQueryFactory queryFactory;
+public class ShowRepositoryImpl extends BaseRepositoryImpl implements ShowRepositoryCustom {
 
     public ShowRepositoryImpl(EntityManager em) {
-        this.em = em;
-        this.queryFactory = new JPAQueryFactory(em);
+        super(em);
     }
 
     @Override
