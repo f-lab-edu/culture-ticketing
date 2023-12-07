d34b25739f69154c82f886239fc866730ed69bec
Joona Choi <juna052@naver.com>
2023-12-07 16:31:38 +0900

    refactor: 장소, 구역과 좌석 컨트롤러 분리

---
+++ src/main/java/com/culture/ticketing/place/api/AreaController.java
@@ -0,0 +1,27 @@
+package com.culture.ticketing.place.api;
+
+import com.culture.ticketing.place.application.AreaService;
+import com.culture.ticketing.place.application.dto.PlaceAreaSaveRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/areas")
+public class AreaController {
+
+    private final AreaService areaService;
+
+    public AreaController(AreaService areaService) {
+        this.areaService = areaService;
+    }
+
+    @PostMapping("")
+    public void postArea(@Valid @RequestBody PlaceAreaSaveRequest request) {
+
+        areaService.createPlaceArea(request);
+    }
+}
--- src/main/java/com/culture/ticketing/place/api/PlaceController.java
+++ src/main/java/com/culture/ticketing/place/api/PlaceController.java
@@ -1,12 +1,8 @@
 package com.culture.ticketing.place.api;
 
-import com.culture.ticketing.place.application.AreaService;
 import com.culture.ticketing.place.application.PlaceService;
-import com.culture.ticketing.place.application.SeatService;
-import com.culture.ticketing.place.application.dto.PlaceAreaSaveRequest;
 import com.culture.ticketing.place.application.dto.PlaceResponse;
 import com.culture.ticketing.place.application.dto.PlaceSaveRequest;
-import com.culture.ticketing.place.application.dto.PlaceSeatSaveRequest;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,13 +13,9 @@ import java.util.List;
 public class PlaceController {
 
     private final PlaceService placeService;
-    private final SeatService seatService;
-    private final AreaService areaService;
 
-    public PlaceController(PlaceService placeService, SeatService seatService, AreaService areaService) {
+    public PlaceController(PlaceService placeService) {
         this.placeService = placeService;
-        this.seatService = seatService;
-        this.areaService = areaService;
     }
 
     @PostMapping("")
@@ -39,14 +31,4 @@ public class PlaceController {
         return placeService.findPlaces(offset, size);
     }
 
-    @PostMapping("/areas")
-    public void postPlaceArea(@Valid @RequestBody PlaceAreaSaveRequest request) {
-
-        areaService.createPlaceArea(request);
-    }
-    @PostMapping("/seats")
-    public void postPlaceSeat(@Valid @RequestBody PlaceSeatSaveRequest request) {
-
-        seatService.createPlaceSeat(request);
-    }
 }
