28ec29ea3d8a219bf38e7ebad68af5cae8ef38e1
Joona Choi <juna052@naver.com>
2024-01-04 21:29:37 +0900

    test: 장소 생성 서비스 테스트 추가

--- src/main/java/com/culture/ticketing/place/application/dto/PlaceSaveRequest.java
+++ src/main/java/com/culture/ticketing/place/application/dto/PlaceSaveRequest.java
@@ -1,6 +1,7 @@
 package com.culture.ticketing.place.application.dto;
 
 import com.culture.ticketing.place.domain.Place;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,6 +23,14 @@ public class PlaceSaveRequest {
     @NotNull
     @Positive
     private BigDecimal longitude;
+
+    @Builder
+    public PlaceSaveRequest(String placeName, String address, BigDecimal latitude, BigDecimal longitude) {
+        this.placeName = placeName;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
     public Place toEntity() {
         return Place.builder()
--- src/test/groovy/com/culture/ticketing/place/application/PlaceServiceTest.groovy
+++ src/test/groovy/com/culture/ticketing/place/application/PlaceServiceTest.groovy
@@ -2,6 +2,7 @@ package com.culture.ticketing.place.application
 
 import com.culture.ticketing.place.PlaceFixtures
 import com.culture.ticketing.place.application.dto.PlaceResponse
+import com.culture.ticketing.place.application.dto.PlaceSaveRequest
 import com.culture.ticketing.place.domain.Place
 import com.culture.ticketing.place.infra.PlaceRepository
 import org.spockframework.spring.SpringBean
@@ -30,5 +31,149 @@ class PlaceServiceTest extends Specification {
 
         then:
         response.collect(place -> place.placeId > 1L).size() == 3
+    }
+
+    def "장소_생성_시_위도가_null_이면_예외_발생"() {
+
+        given:
+        PlaceSaveRequest request = PlaceSaveRequest.builder()
+                .placeName("테스트")
+                .address("서울특별시")
+                .latitude(null)
+                .longitude(new BigDecimal(0))
+                .build();
+
+        when:
+        placeService.createPlace(request);
+
+        then:
+        def e = thrown(NullPointerException.class)
+        e.message == "정확한 장소 위도를 입력해주세요."
+    }
+
+    def "장소_생성_시_경도가_null_이면_예외_발생"() {
+
+        given:
+        PlaceSaveRequest request = PlaceSaveRequest.builder()
+                .placeName("테스트")
+                .address("서울특별시")
+                .latitude(new BigDecimal(0))
+                .longitude(null)
+                .build();
+
+        when:
+        placeService.createPlace(request);
+
+        then:
+        def e = thrown(NullPointerException.class)
+        e.message == "정확한 장소 경도를 입력해주세요."
+    }
+
+    def "장소_생성_시_장소_주소가_null_이면_예외_발생"() {
+
+        given:
+        PlaceSaveRequest request = PlaceSaveRequest.builder()
+                .placeName("테스트")
+                .address(null)
+                .latitude(new BigDecimal(0))
+                .longitude(new BigDecimal(0))
+                .build();
+
+        when:
+        placeService.createPlace(request);
+
+        then:
+        def e = thrown(IllegalArgumentException.class)
+        e.message == "장소 주소를 입력해주세요."
+    }
+
+    def "장소_생성_시_장소 주소가_빈_값_이면_예외_발생"() {
+
+        given:
+        PlaceSaveRequest request = PlaceSaveRequest.builder()
+                .placeName("테스트")
+                .address("")
+                .latitude(new BigDecimal(0))
+                .longitude(new BigDecimal(0))
+                .build();
+
+        when:
+        placeService.createPlace(request);
+
+        then:
+        def e = thrown(IllegalArgumentException.class)
+        e.message == "장소 주소를 입력해주세요."
+    }
+
+    def "장소_생성_시_위도_범위가_-90_미만인_경우_예외_발생"() {
+
+        given:
+        PlaceSaveRequest request = PlaceSaveRequest.builder()
+                .placeName("테스트")
+                .address("서울특별시")
+                .latitude(new BigDecimal(-91))
+                .longitude(new BigDecimal(0))
+                .build();
+
+        when:
+        placeService.createPlace(request);
+
+        then:
+        def e = thrown(IllegalArgumentException.class)
+        e.message == "장소 위도 범위를 벗어난 입력값입니다."
+    }
+
+    def "장소_생성_시_위도_범위가_90_초과인_경우_예외_발생"() {
+
+        given:
+        PlaceSaveRequest request = PlaceSaveRequest.builder()
+                .placeName("테스트")
+                .address("서울특별시")
+                .latitude(new BigDecimal(91))
+                .longitude(new BigDecimal(0))
+                .build();
+
+        when:
+        placeService.createPlace(request);
+
+        then:
+        def e = thrown(IllegalArgumentException.class)
+        e.message == "장소 위도 범위를 벗어난 입력값입니다."
+    }
+
+    def "장소_생성_시_경도_범위가_-180_미만인_경우_예외_발생"() {
+
+        given:
+        PlaceSaveRequest request = PlaceSaveRequest.builder()
+                .placeName("테스트")
+                .address("서울특별시")
+                .latitude(new BigDecimal(0))
+                .longitude(new BigDecimal(-181))
+                .build();
+
+        when:
+        placeService.createPlace(request);
+
+        then:
+        def e = thrown(IllegalArgumentException.class)
+        e.message == "장소 경도 범위를 벗어난 입력값입니다."
+    }
+
+    def "장소_생성_시_경도_범위가_180_초과인_경우_예외_발생"() {
+
+        given:
+        PlaceSaveRequest request = PlaceSaveRequest.builder()
+                .placeName("테스트")
+                .address("서울특별시")
+                .latitude(new BigDecimal(0))
+                .longitude(new BigDecimal(181))
+                .build();
+
+        when:
+        placeService.createPlace(request);
+
+        then:
+        def e = thrown(IllegalArgumentException.class)
+        e.message == "장소 경도 범위를 벗어난 입력값입니다."
     }
 }
