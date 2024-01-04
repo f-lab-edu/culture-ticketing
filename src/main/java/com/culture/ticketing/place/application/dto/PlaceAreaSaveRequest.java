9b9a3df51e6aff0ddbb60da1f2063e934a56c88a
Joona Choi <juna052@naver.com>
2024-01-04 23:02:28 +0900

    test: 장소 구역 생성 서비스 테스트 추가

--- src/main/java/com/culture/ticketing/place/application/dto/PlaceAreaSaveRequest.java
+++ src/main/java/com/culture/ticketing/place/application/dto/PlaceAreaSaveRequest.java
@@ -1,6 +1,7 @@
 package com.culture.ticketing.place.application.dto;
 
 import com.culture.ticketing.place.domain.Area;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,12 @@ public class PlaceAreaSaveRequest {
     private String areaName;
     @NotNull
     private Long placeId;
+
+    @Builder
+    public PlaceAreaSaveRequest(String areaName, Long placeId) {
+        this.areaName = areaName;
+        this.placeId = placeId;
+    }
 
     public Area toEntity() {
         return Area.builder()
---
+++ src/test/groovy/com/culture/ticketing/place/application/AreaServiceTest.groovy
@@ -0,0 +1,50 @@
+package com.culture.ticketing.place.application
+
+import com.culture.ticketing.place.application.dto.PlaceAreaSaveRequest
+import com.culture.ticketing.place.exception.PlaceNotFoundException
+import com.culture.ticketing.place.infra.AreaRepository
+import org.spockframework.spring.SpringBean
+import spock.lang.Specification
+
+class AreaServiceTest extends Specification {
+
+    @SpringBean
+    private AreaRepository areaRepository = Mock();
+    @SpringBean
+    private PlaceService placeService = Mock();
+    private AreaService areaService = new AreaService(areaRepository, placeService);
+
+    def "구역_생성_시_장소_아이디가_null_인_경우_예외_발생"() {
+
+        given:
+        PlaceAreaSaveRequest request = PlaceAreaSaveRequest.builder()
+                .areaName("테스트")
+                .placeId(null)
+                .build();
+
+        when:
+        areaService.createPlaceArea(request);
+
+        then:
+        def e = thrown(NullPointerException.class)
+        e.message == "장소 아이디를 입력해주세요."
+    }
+
+    def "구역_생성_시_장소_아이디가_존재하지_않는_경우_예외_발생"() {
+
+        given:
+        Long placeId = 1000L;
+        PlaceAreaSaveRequest request = PlaceAreaSaveRequest.builder()
+                .areaName("테스트")
+                .placeId(placeId)
+                .build();
+        placeService.notExistsById(placeId) >> true
+
+        when:
+        areaService.createPlaceArea(request);
+
+        then:
+        def e = thrown(PlaceNotFoundException.class)
+        e.message == String.format("존재하지 않는 장소입니다. (placeId = %d)", placeId)
+    }
+}
