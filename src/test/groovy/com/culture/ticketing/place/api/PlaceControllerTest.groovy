5fa8ce3879cdba328856efaaf1a9f57c33c0d409
Joona Choi <juna052@naver.com>
2024-01-08 23:57:29 +0900

    test: 컨트롤러 테스트 mock user 어노테이션 추가

--- src/test/groovy/com/culture/ticketing/place/api/PlaceControllerTest.groovy
+++ src/test/groovy/com/culture/ticketing/place/api/PlaceControllerTest.groovy
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(PlaceController.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@WithMockUser
 class PlaceControllerTest extends Specification {
 
     @Autowired
