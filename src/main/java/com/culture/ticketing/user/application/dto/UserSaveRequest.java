f7c4cff06f1e3c1f8c7b0f1ea21174f416fb2535
Joona Choi <juna052@naver.com>
2024-01-08 23:56:31 +0900

    test: 유저 생성 도메인 및 서비스 테스트 추가

--- src/main/java/com/culture/ticketing/user/application/dto/UserSaveRequest.java
+++ src/main/java/com/culture/ticketing/user/application/dto/UserSaveRequest.java
@@ -1,6 +1,7 @@
 package com.culture.ticketing.user.application.dto;
 
 import com.culture.ticketing.user.domain.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,6 +19,14 @@ public class UserSaveRequest {
     private String userName;
     @NotBlank
     private String phoneNumber;
+
+    @Builder
+    public UserSaveRequest(String email, String password, String userName, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+        this.phoneNumber = phoneNumber;
+    }
 
     public User toEntity() {
         return User.builder()
