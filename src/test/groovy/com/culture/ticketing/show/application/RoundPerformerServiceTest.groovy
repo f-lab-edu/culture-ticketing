067484e9317f762f9fd6e0755175c8ec8230a2a4
Joona Choi <juna052@naver.com>
2024-01-10 23:12:02 +0900

    test: 회차 출연자 목록 생성 서비스 성공 테스트 추가

--- src/test/groovy/com/culture/ticketing/show/application/RoundPerformerServiceTest.groovy
+++ src/test/groovy/com/culture/ticketing/show/application/RoundPerformerServiceTest.groovy
@@ -1,5 +1,6 @@
 package com.culture.ticketing.show.application
 
+import com.culture.ticketing.show.RoundFixtures
 import com.culture.ticketing.show.application.dto.RoundPerformersSaveRequest
 import com.culture.ticketing.show.infra.RoundPerformerRepository
 import org.spockframework.spring.SpringBean
@@ -45,5 +46,21 @@ class RoundPerformerServiceTest extends Specification {
         then:
         def e = thrown(NullPointerException.class)
         e.message == "출연자 아이디 목록을 입력해주세요."
+    }
+
+    def "회차_출연자_목록_생성_성공"() {
+
+        given:
+        RoundPerformersSaveRequest request = RoundPerformersSaveRequest.builder()
+                .roundId(1L)
+                .performerIds(Set.of(1L, 2L, 3L))
+                .build();
+        roundService.findById(1L) >> RoundFixtures.createRound(1L)
+
+        when:
+        roundPerformerService.createRoundPerformers(request);
+
+        then:
+        1 * roundPerformerRepository.saveAll(_)
     }
 }
