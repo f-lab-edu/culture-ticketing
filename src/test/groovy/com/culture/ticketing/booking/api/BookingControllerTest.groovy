13a90aab95d2b6d9770f7792ea14d8bc2e5e0915
Joona Choi <juna052@naver.com>
2024-01-26 23:04:30 +0900

    test: 예약 컨트롤러 서비스 내 authority 설정 어노테이션 제거

--- src/test/groovy/com/culture/ticketing/booking/api/BookingControllerTest.groovy
+++ src/test/groovy/com/culture/ticketing/booking/api/BookingControllerTest.groovy
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
 import org.springframework.http.MediaType
-import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
@@ -21,7 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(BookingController.class)
 @MockBean(JpaMetamodelMappingContext.class)
-@WithMockUser
 class BookingControllerTest extends Specification {
 
     @Autowired
