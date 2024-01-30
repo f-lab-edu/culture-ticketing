feacaaf7df9c968659d93d42bff429fa3aab5e50
Joona Choi <juna052@naver.com>
2024-01-30 18:51:20 +0900

    feat: 공연 아이디에 해당하는 공연 좌석 등급 목록 조회 기능 추가

--- src/main/java/com/culture/ticketing/show/api/ShowSeatGradeController.java
+++ src/main/java/com/culture/ticketing/show/api/ShowSeatGradeController.java
@@ -1,13 +1,17 @@
 package com.culture.ticketing.show.api;
 
 import com.culture.ticketing.show.application.ShowSeatGradeService;
+import com.culture.ticketing.show.application.dto.ShowSeatGradeResponse;
 import com.culture.ticketing.show.application.dto.ShowSeatGradeSaveRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/show-seat-grades")
@@ -23,5 +27,11 @@ public class ShowSeatGradeController {
     public void postShowSeatGrade(@Valid @RequestBody ShowSeatGradeSaveRequest request) {
 
         showSeatGradeService.createShowSeatGrade(request);
+    }
+
+    @GetMapping
+    public List<ShowSeatGradeResponse> getShowSeatGradesByShowId(@RequestParam("showId") Long showId) {
+
+        return showSeatGradeService.findShowSeatGradesByShowId(showId);
     }
 }
