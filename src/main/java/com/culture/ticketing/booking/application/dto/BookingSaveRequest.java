ab919b7a53f487a15218fdb164018dfc27c074cd
Joona Choi <juna052@naver.com>
2024-02-15 16:35:19 +0900

    fix: 요청값으로부터 예약 엔티티 생성 시 연관 관계 추가하도록 수정

--- src/main/java/com/culture/ticketing/booking/application/dto/BookingSaveRequest.java
+++ src/main/java/com/culture/ticketing/booking/application/dto/BookingSaveRequest.java
@@ -1,12 +1,16 @@
 package com.culture.ticketing.booking.application.dto;
 
 import com.culture.ticketing.booking.domain.Booking;
+import com.culture.ticketing.booking.domain.BookingShowFloor;
+import com.culture.ticketing.booking.domain.BookingShowSeat;
 import com.culture.ticketing.booking.domain.BookingStatus;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -15,8 +19,8 @@ public class BookingSaveRequest {
     private Long userId;
     private Long roundId;
     private int totalPrice;
-    private List<Long> showSeatIds;
-    private List<BookingShowFloorSaveRequest> showFloors;
+    private List<Long> showSeatIds = new ArrayList<>();
+    private List<BookingShowFloorSaveRequest> showFloors = new ArrayList<>();
 
     @Builder
     public BookingSaveRequest(Long userId, Long roundId, int totalPrice, List<Long> showSeatIds, List<BookingShowFloorSaveRequest> showFloors) {
@@ -28,12 +32,31 @@ public class BookingSaveRequest {
     }
 
     public Booking toEntity() {
-        return Booking.builder()
+        Booking booking = Booking.builder()
                 .userId(userId)
                 .roundId(roundId)
                 .totalPrice(totalPrice)
                 .bookingStatus(BookingStatus.SUCCESS)
                 .build();
+
+        List<BookingShowSeat> bookingShowSeats = showSeatIds.stream()
+                .map(showSeatId -> BookingShowSeat.builder()
+                        .showSeatId(showSeatId)
+                        .booking(booking)
+                        .build())
+                .collect(Collectors.toList());
+        booking.setBookingShowSeats(bookingShowSeats);
+
+        List<BookingShowFloor> bookingShowFloors = showFloors.stream()
+                .map(showFloor -> BookingShowFloor.builder()
+                        .showFloorId(showFloor.getShowFloorId())
+                        .entryOrder(showFloor.getEntryOrder())
+                        .booking(booking)
+                        .build())
+                .collect(Collectors.toList());
+        booking.setBookingShowFloors(bookingShowFloors);
+
+        return booking;
     }
 
 }
