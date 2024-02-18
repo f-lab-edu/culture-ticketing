a22e331738ab5a0880f38042b57cdd344faec5c4
Joona Choi <juna052@naver.com>
2024-02-18 12:45:31 +0900

    fix: 패키지 구조 변경에 따른 import 오류 수정

--- src/main/java/com/culture/ticketing/show/round_performer/infra/RoundRepositoryImpl.java
+++ src/main/java/com/culture/ticketing/show/round_performer/infra/RoundRepositoryImpl.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static com.culture.ticketing.show.domain.QRound.round;
+import static com.culture.ticketing.show.round_performer.domain.QRound.round;
 
 
 public class RoundRepositoryImpl extends BaseRepositoryImpl implements RoundRepositoryCustom {
