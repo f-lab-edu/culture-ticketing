bf9453aea51dd004fbab9de1ed3fe28afffd433a
Joona Choi <juna052@naver.com>
2024-02-21 17:18:39 +0900

    test: 회차 아이디와 구역 아이디로 공연 좌석 정보 목록 조회 테스트 추가

--- src/test/groovy/com/culture/ticketing/show/show_seat/api/ShowSeatControllerTest.groovy
+++ src/test/groovy/com/culture/ticketing/show/show_seat/api/ShowSeatControllerTest.groovy
@@ -1,8 +1,14 @@
 package com.culture.ticketing.show.show_seat.api
 
+import com.culture.ticketing.place.SeatFixtures
+import com.culture.ticketing.show.show_seat.ShowSeatFixtures
+import com.culture.ticketing.show.show_seat.ShowSeatGradeFixtures
+import com.culture.ticketing.show.show_seat.application.ShowSeatFacadeService
 import com.culture.ticketing.show.show_seat.application.ShowSeatService
+import com.culture.ticketing.show.show_seat.application.dto.ShowSeatResponse
 import com.culture.ticketing.show.show_seat.application.dto.ShowSeatSaveRequest
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.hamcrest.Matchers
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -14,6 +20,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import spock.lang.Specification
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(ShowSeatController.class)
@@ -26,6 +37,8 @@ class ShowSeatControllerTest extends Specification {
     private ObjectMapper objectMapper;
     @SpringBean
     private ShowSeatService showSeatService = Mock();
+    @SpringBean
+    private ShowSeatFacadeService showSeatFacadeService = Mock();
 
     def "공연 좌석 정보 생성 성공"() {
 
@@ -44,4 +57,37 @@ class ShowSeatControllerTest extends Specification {
                 .andDo(MockMvcResultHandlers.print())
     }
 
+    def "회차 아이디와 구역 아이디로 공연 좌석 목록 조회"() {
+
+        given:
+        showSeatFacadeService.findByRoundIdAndAreaId(1L, 1L) >> [
+                new ShowSeatResponse(
+                        ShowSeatFixtures.createShowSeat(showSeatId: 1L, seatId: 1L, showSeatGradeId: 1L),
+                        SeatFixtures.creatSeat(seatId: 1L),
+                        ShowSeatGradeFixtures.createShowSeatGrade(showSeatGradeId: 1L),
+                        true
+                ),
+                new ShowSeatResponse(
+                        ShowSeatFixtures.createShowSeat(showSeatId: 2L, seatId: 2L, showSeatGradeId: 1L),
+                        SeatFixtures.creatSeat(seatId: 2L),
+                        ShowSeatGradeFixtures.createShowSeatGrade(showSeatGradeId: 1L),
+                        false
+                ),
+        ]
+
+        expect:
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/show-seats")
+                .param("roundId", "1")
+                .param("areaId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("\$").exists())
+                .andExpect(jsonPath("\$", Matchers.hasSize(2)))
+                .andExpect(jsonPath("\$[0].showSeatId").value(1L))
+                .andExpect(jsonPath("\$[0].seatId").value(1L))
+                .andExpect(jsonPath("\$[0].showSeatGradeId").value(1L))
+                .andExpect(jsonPath("\$[1].showSeatId").value(2L))
+                .andExpect(jsonPath("\$[1].seatId").value(2L))
+                .andExpect(jsonPath("\$[1].showSeatGradeId").value(1L))
+                .andDo(MockMvcResultHandlers.print())
+    }
 }
