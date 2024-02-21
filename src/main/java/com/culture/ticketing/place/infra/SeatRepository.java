cff9a4153cbaca06c83472fe26eb302c5037e787
Joona Choi <juna052@naver.com>
2024-02-21 16:16:40 +0900

    feat: 좌석 아이디 목록으로 좌석 목록 조회 기능 추가

--- src/main/java/com/culture/ticketing/place/infra/SeatRepository.java
+++ src/main/java/com/culture/ticketing/place/infra/SeatRepository.java
@@ -4,14 +4,10 @@ import com.culture.ticketing.place.domain.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
     Optional<Seat> findByAreaIdAndSeatRowAndSeatNumber(Long areaId, int seatRow, int seatNumber);
-
-    List<Seat> findBySeatIdIn(Collection<Long> seatIds);
 }
