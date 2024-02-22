352494e2608eafbcb82fcc94c0b17445c4a8b4f1
Joona Choi <juna052@naver.com>
2024-02-22 19:54:56 +0900

    chore: 장소 컨트롤러 설명 swagger annotation 추가

--- src/main/java/com/culture/ticketing/place/api/PlaceController.java
+++ src/main/java/com/culture/ticketing/place/api/PlaceController.java
@@ -3,10 +3,14 @@ package com.culture.ticketing.place.api;
 import com.culture.ticketing.place.application.PlaceService;
 import com.culture.ticketing.place.application.dto.PlaceResponse;
 import com.culture.ticketing.place.application.dto.PlaceSaveRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"장소 Controller"})
 @RestController
 @RequestMapping("/api/v1/places")
 public class PlaceController {
@@ -17,15 +21,17 @@ public class PlaceController {
         this.placeService = placeService;
     }
 
+    @ApiOperation(value = "장소 생성 API")
     @PostMapping
     public void postPlace(@RequestBody PlaceSaveRequest request) {
 
         placeService.createPlace(request);
     }
 
+    @ApiOperation(value = "장소 목록 조회 API")
     @GetMapping
-    public List<PlaceResponse> getPlaces(@RequestParam(name = "offset") Long offset,
-                                         @RequestParam(name = "size") int size) {
+    public List<PlaceResponse> getPlaces(@ApiParam(value = "처음 시작 위치") @RequestParam(name = "offset") Long offset,
+                                         @ApiParam(value = "사이즈") @RequestParam(name = "size") int size) {
 
         return placeService.findPlaces(offset, size);
     }
--- src/main/java/com/culture/ticketing/place/application/dto/PlaceSaveRequest.java
+++ src/main/java/com/culture/ticketing/place/application/dto/PlaceSaveRequest.java
@@ -1,19 +1,25 @@
 package com.culture.ticketing.place.application.dto;
 
 import com.culture.ticketing.place.domain.Place;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Schema(description = "장소 생성 요청 DTO")
 @Getter
 @NoArgsConstructor
 public class PlaceSaveRequest {
 
+    @Schema(description = "장소명")
     private String placeName;
+    @Schema(description = "주소")
     private String address;
+    @Schema(description = "위도")
     private BigDecimal latitude;
+    @Schema(description = "경도")
     private BigDecimal longitude;
 
     @Builder
