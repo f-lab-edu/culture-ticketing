553eaeeca3ab29fdddfe4c44874a0fec1c1f4cf0
Joona Choi <juna052@naver.com>
2024-02-22 19:58:12 +0900

    chore: 장소 좌석 컨트롤러 설명 swagger annotation 추가

--- src/main/java/com/culture/ticketing/place/api/SeatController.java
+++ src/main/java/com/culture/ticketing/place/api/SeatController.java
@@ -2,11 +2,14 @@ package com.culture.ticketing.place.api;
 
 import com.culture.ticketing.place.application.SeatService;
 import com.culture.ticketing.place.application.dto.PlaceSeatSaveRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"장소 좌석 Controller"})
 @RestController
 @RequestMapping("/api/v1/seats")
 public class SeatController {
@@ -17,6 +20,7 @@ public class SeatController {
         this.seatService = seatService;
     }
 
+    @ApiOperation(value = "장소 좌석 생성 API")
     @PostMapping
     public void postPlaceSeat(@RequestBody PlaceSeatSaveRequest request) {
 
--- src/main/java/com/culture/ticketing/place/application/dto/PlaceSeatSaveRequest.java
+++ src/main/java/com/culture/ticketing/place/application/dto/PlaceSeatSaveRequest.java
@@ -1,16 +1,21 @@
 package com.culture.ticketing.place.application.dto;
 
 import com.culture.ticketing.place.domain.Seat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "장소 좌석 생성 요청 DTO")
 @Getter
 @NoArgsConstructor
 public class PlaceSeatSaveRequest {
 
+    @Schema(description = "좌석 열")
     private int seatRow;
+    @Schema(description = "좌석 번호")
     private int seatNumber;
+    @Schema(description = "구역 아이디")
     private Long areaId;
 
     @Builder
