54181ef359c08aed6566543a24b08743cb2a67be
Joona Choi <juna052@naver.com>
2024-02-22 18:42:59 +0900

    chore: 장소 구역 컨트롤러 설명 swagger annotation 추가

--- src/main/java/com/culture/ticketing/place/api/AreaController.java
+++ src/main/java/com/culture/ticketing/place/api/AreaController.java
@@ -2,11 +2,14 @@ package com.culture.ticketing.place.api;
 
 import com.culture.ticketing.place.application.AreaService;
 import com.culture.ticketing.place.application.dto.PlaceAreaSaveRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"장소 구역 Controller"})
 @RestController
 @RequestMapping("/api/v1/areas")
 public class AreaController {
@@ -17,6 +20,7 @@ public class AreaController {
         this.areaService = areaService;
     }
 
+    @ApiOperation(value = "장소 구역 생성 API")
     @PostMapping
     public void postArea(@RequestBody PlaceAreaSaveRequest request) {
 
--- src/main/java/com/culture/ticketing/place/application/dto/PlaceAreaSaveRequest.java
+++ src/main/java/com/culture/ticketing/place/application/dto/PlaceAreaSaveRequest.java
@@ -1,15 +1,19 @@
 package com.culture.ticketing.place.application.dto;
 
 import com.culture.ticketing.place.domain.Area;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "장소 구역 생성 요청 DTO")
 @Getter
 @NoArgsConstructor
 public class PlaceAreaSaveRequest {
 
+    @Schema(description = "구역명")
     private String areaName;
+    @Schema(description = "장소 아이디")
     private Long placeId;
 
     @Builder
