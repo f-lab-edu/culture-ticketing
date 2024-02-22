87938d688ca6748055d712892037b0d3c0d9651f
Joona Choi <juna052@naver.com>
2024-02-22 20:09:44 +0900

    chore: 장소 응답 DTO 설명 swagger annotation 추가

--- src/main/java/com/culture/ticketing/place/application/dto/PlaceResponse.java
+++ src/main/java/com/culture/ticketing/place/application/dto/PlaceResponse.java
@@ -1,17 +1,24 @@
 package com.culture.ticketing.place.application.dto;
 
 import com.culture.ticketing.place.domain.Place;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
+@Schema(description = "장소 응답 DTO")
 @Getter
 public class PlaceResponse {
 
+    @Schema(description = "장소 아이디")
     private final Long placeId;
+    @Schema(description = "장소명")
     private final String placeName;
+    @Schema(description = "주소")
     private final String address;
+    @Schema(description = "위도")
     private final BigDecimal latitude;
+    @Schema(description = "경도")
     private final BigDecimal longitude;
 
     public PlaceResponse(Place place) {
