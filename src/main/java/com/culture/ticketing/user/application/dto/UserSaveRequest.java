c90ad22bdd7fbcdebcef1a96290288f255c564c3
Joona Choi <juna052@naver.com>
2024-02-22 20:01:03 +0900

    chore: 회원 컨트롤러 설명 swagger annotation 추가

--- src/main/java/com/culture/ticketing/user/application/dto/UserSaveRequest.java
+++ src/main/java/com/culture/ticketing/user/application/dto/UserSaveRequest.java
@@ -1,18 +1,24 @@
 package com.culture.ticketing.user.application.dto;
 
 import com.culture.ticketing.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Schema(description = "회원 생성 요청 DTO")
 @Getter
 @NoArgsConstructor
 public class UserSaveRequest {
 
+    @Schema(description = "이메일")
     private String email;
+    @Schema(description = "비밀번호")
     private String password;
+    @Schema(description = "회원 이름")
     private String userName;
+    @Schema(description = "연락처")
     private String phoneNumber;
 
     @Builder
