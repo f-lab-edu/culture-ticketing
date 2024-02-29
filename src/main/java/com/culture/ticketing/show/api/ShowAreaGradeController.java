91c29cd1591aa3efc03d18fc9d25b1c5bc47d206
Joona Choi <juna052@naver.com>
2024-02-29 16:18:35 +0900

    refactor: 공연 구역 등급 생성 및 조회 기능 수정
    - 공연 좌석/플로어 등급 구조 제거 후 구역 등급으로 통합

---
+++ src/main/java/com/culture/ticketing/show/api/ShowAreaGradeController.java
@@ -0,0 +1,36 @@
+package com.culture.ticketing.show.api;
+
+import com.culture.ticketing.show.application.ShowAreaGradeService;
+import com.culture.ticketing.show.application.dto.ShowAreaGradeResponse;
+import com.culture.ticketing.show.application.dto.ShowAreaGradeSaveRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/show-area-grades")
+public class ShowAreaGradeController {
+
+    private final ShowAreaGradeService showAreaGradeService;
+
+    public  ShowAreaGradeController(ShowAreaGradeService showAreaGradeService) {
+        this.showAreaGradeService = showAreaGradeService;
+    }
+
+    @PostMapping
+    public void postShowAreaGrade(@RequestBody ShowAreaGradeSaveRequest request) {
+
+        showAreaGradeService.createShowAreaGrade(request);
+    }
+
+    @GetMapping
+    public List<ShowAreaGradeResponse> getShowAreaGradesByShowId(@RequestParam("showId") Long showId) {
+
+        return showAreaGradeService.findShowAreaGradesByShowId(showId);
+    }
+}
