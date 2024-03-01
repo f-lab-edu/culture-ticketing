89326275952361f2b9de3933050d6eac7c74f43c
Joona Choi <juna052@naver.com>
2024-03-01 21:27:21 +0900

    fix: QPlace import 위치 수정

--- src/main/java/com/culture/ticketing/show/infra/PlaceRepositoryImpl.java
+++ src/main/java/com/culture/ticketing/show/infra/PlaceRepositoryImpl.java
@@ -6,7 +6,7 @@ import com.culture.ticketing.show.domain.Place;
 import javax.persistence.EntityManager;
 import java.util.List;
 
-import static com.culture.ticketing.place.domain.QPlace.place;
+import static com.culture.ticketing.show.domain.QPlace.place;
 
 public class PlaceRepositoryImpl extends BaseRepositoryImpl implements PlaceRepositoryCustom {
 
