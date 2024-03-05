6db8b9d185c6af2d03b359e817995df48672238b
Joona Choi <juna052@naver.com>
2024-03-05 16:25:45 +0900

    chore: 공연 좌석 관련 swagger annotation 추가

--- src/main/java/com/culture/ticketing/show/api/ShowSeatController.java
+++ src/main/java/com/culture/ticketing/show/api/ShowSeatController.java
@@ -4,6 +4,9 @@ import com.culture.ticketing.show.application.ShowFacadeService;
 import com.culture.ticketing.show.application.ShowSeatService;
 import com.culture.ticketing.show.application.dto.ShowSeatResponse;
 import com.culture.ticketing.show.application.dto.ShowSeatSaveRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = {"공연 좌석 Controller"})
 @RestController
 @RequestMapping("/api/v1/show-seats")
 public class ShowSeatController {
@@ -25,14 +29,17 @@ public class ShowSeatController {
         this.showFacadeService = showFacadeService;
     }
 
+    @ApiOperation(value = "공연 좌석 생성 API")
     @PostMapping
     public void postShowSeat(@RequestBody ShowSeatSaveRequest request) {
 
         showSeatService.createShowSeat(request);
     }
 
+    @ApiOperation(value = "공연 좌석 목록 조회 API")
     @GetMapping
-    public List<ShowSeatResponse> getShowSeatsByShowAreaIdAndRoundId(@RequestParam("showAreaId") Long showAreaId, @RequestParam("roundId") Long roundId) {
+    public List<ShowSeatResponse> getShowSeatsByShowAreaIdAndRoundId(@ApiParam(value = "공연 구역 아이디") @RequestParam("showAreaId") Long showAreaId,
+                                                                     @ApiParam(value = "회차 아이디") @RequestParam("roundId") Long roundId) {
 
         return showFacadeService.findShowSeatsByShowAreaIdAndRoundId(showAreaId, roundId);
     }
--- src/main/java/com/culture/ticketing/show/application/dto/ShowSeatResponse.java
+++ src/main/java/com/culture/ticketing/show/application/dto/ShowSeatResponse.java
@@ -1,14 +1,20 @@
 package com.culture.ticketing.show.application.dto;
 
 import com.culture.ticketing.show.domain.ShowSeat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(description = "공연 좌석 응답 DTO")
 @Getter
 public class ShowSeatResponse {
 
+    @Schema(description = "공연 좌석 아이디")
     private final Long showSeatId;
+    @Schema(description = "공연 좌석 열")
     private final String showSeatRow;
+    @Schema(description = "공연 좌석 번호")
     private final int showSeatNumber;
+    @Schema(description = "예약 가능 여부")
     private final Boolean isAvailable;
 
     public ShowSeatResponse(ShowSeat showSeat, Boolean isAvailable) {
