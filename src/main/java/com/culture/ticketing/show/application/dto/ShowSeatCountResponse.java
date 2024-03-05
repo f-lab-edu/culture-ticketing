9f50e562d63f162fc750ed6983e13ce94419e522
Joona Choi <juna052@naver.com>
2024-03-05 16:30:00 +0900

    chore: 회차 및 출연자, 공연 구역 등급 목록 응답 DTO swagger annotation 추가

--- src/main/java/com/culture/ticketing/show/application/dto/ShowSeatCountResponse.java
+++ src/main/java/com/culture/ticketing/show/application/dto/ShowSeatCountResponse.java
@@ -1,14 +1,20 @@
 package com.culture.ticketing.show.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
+@Schema(description = "공연 구역 등급별 이용 가능 공연 좌석 수 응답 DTO")
 @Getter
 public class ShowSeatCountResponse {
 
+    @Schema(description = "공연 구역 등급 아이디")
     private final Long showAreaGradeId;
+    @Schema(description = "공연 구역 등급명")
     private final String showAreaGradeName;
+    @Schema(description = "가격")
     private final int price;
+    @Schema(description = "이용 가능 공연 좌석 수")
     private long availableSeatCount = 0;
 
     @Builder
--- src/main/java/com/culture/ticketing/show/round_performer/application/dto/RoundWithPerformersAndShowAreaGradesResponse.java
+++ src/main/java/com/culture/ticketing/show/round_performer/application/dto/RoundWithPerformersAndShowAreaGradesResponse.java
@@ -2,18 +2,25 @@ package com.culture.ticketing.show.round_performer.application.dto;
 
 import com.culture.ticketing.show.application.dto.ShowSeatCountResponse;
 import com.culture.ticketing.show.application.dto.ShowSeatCountsResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Schema(description = "회차 및 출연자, 공연 구역 등급 정보 응답 DTO")
 @Getter
 public class RoundWithPerformersAndShowAreaGradesResponse {
 
+    @Schema(description = "회차 아이디")
     private final Long roundId;
+    @Schema(description = "회차 시작 일시")
     private final LocalDateTime roundStartDateTime;
+    @Schema(description = "회차 종료 일시")
     private final LocalDateTime roundEndDateTime;
+    @Schema(description = "출연자 목록")
     private final List<PerformerResponse> performers;
+    @Schema(description = "공연 구역 등급별 이용 가능 공연 좌석 갯수 목록")
     private final List<ShowSeatCountResponse> showSeatCounts;
 
     public RoundWithPerformersAndShowAreaGradesResponse(RoundWithPerformersResponse roundWithPerformers, ShowSeatCountsResponse showSeatCounts) {
