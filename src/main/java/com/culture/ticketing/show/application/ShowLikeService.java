3670ba57c64e5b884b38dbb235099d16adc01b66
Joona Choi <juna052@naver.com>
2024-04-03 11:37:12 +0900

    fix: 공연 좋아요 생성/삭제 시 RDB 가 아닌 Redis 사용하도록 수정

--- src/main/java/com/culture/ticketing/show/application/ShowLikeService.java
+++ src/main/java/com/culture/ticketing/show/application/ShowLikeService.java
@@ -1,24 +1,28 @@
 package com.culture.ticketing.show.application;
 
 import com.culture.ticketing.show.application.dto.ShowResponse;
-import com.culture.ticketing.show.domain.ShowLike;
-import com.culture.ticketing.show.infra.ShowLikeRepository;
+import com.culture.ticketing.user.application.UserService;
 import com.culture.ticketing.user.domain.User;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class ShowLikeService {
 
-    private final ShowLikeRepository showLikeRepository;
     private final ShowService showService;
+    private final UserService userService;
+    private final RedisTemplate<Long, Set<Long>> redisTemplate;
 
-    public ShowLikeService(ShowLikeRepository showLikeRepository, ShowService showService) {
-        this.showLikeRepository = showLikeRepository;
+    public ShowLikeService(ShowService showService, UserService userService, RedisTemplate<Long, Set<Long>> redisTemplate) {
         this.showService = showService;
+        this.userService = userService;
+        this.redisTemplate = redisTemplate;
     }
 
     @Transactional
@@ -26,25 +30,31 @@ public class ShowLikeService {
 
         ShowResponse show = showService.findShowById(showId);
 
-        ShowLike showLike = showLikeRepository.findByUserIdAndShowId(user.getUserId(), showId)
-                .orElseGet(() -> ShowLike.builder()
-                        .userId(user.getUserId())
-                        .showId(show.getShowId())
-                        .build());
-
-        showLikeRepository.save(showLike);
+        ValueOperations<Long, Set<Long>> valueOperations = redisTemplate.opsForValue();
+        Set<Long> userIds = valueOperations.get(show.getShowId());
+        if (userIds == null) {
+            userIds = new HashSet<>();
+        }
+        userIds.add(user.getUserId());
+        valueOperations.set(show.getShowId(), userIds);
     }
 
     @Transactional
     public void deleteShowLike(User user, Long showId) {
 
-        showLikeRepository.findByUserIdAndShowId(user.getUserId(), showId)
-                .ifPresent(showLikeRepository::delete);
+        ValueOperations<Long, Set<Long>> valueOperations = redisTemplate.opsForValue();
+        Set<Long> userIds = valueOperations.get(showId);
+        if (userIds != null) {
+            userIds.remove(user.getUserId());
+            valueOperations.set(showId, userIds);
+        }
     }
 
     @Transactional(readOnly = true)
-    public List<ShowLike> findByShowIds(Collection<Long> showIds) {
+    public int countShowLikesByShowId(Long showId) {
 
-        return showLikeRepository.findByShowIdIn(showIds);
+        Set<Long> userIds = redisTemplate.opsForValue().get(showId);
+
+        return userIds == null ? 0 : userIds.size();
     }
 }
