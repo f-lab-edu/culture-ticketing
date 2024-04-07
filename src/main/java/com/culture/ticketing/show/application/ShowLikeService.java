43c3ea98350b32c584e28bba8e4e9eda7660cb20
Joona Choi <juna052@naver.com>
2024-04-07 09:16:42 +0900

    fix: redis 를 set 자료구조로 사용하도록 변경

--- src/main/java/com/culture/ticketing/show/application/ShowLikeService.java
+++ src/main/java/com/culture/ticketing/show/application/ShowLikeService.java
@@ -1,67 +1,48 @@
 package com.culture.ticketing.show.application;
 
-import com.culture.ticketing.show.application.dto.ShowResponse;
-import com.culture.ticketing.user.application.UserService;
 import com.culture.ticketing.user.domain.User;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Service
 public class ShowLikeService {
 
-    private final ShowService showService;
-    private final UserService userService;
-    private final RedisTemplate<Long, Set<Long>> redisTemplate;
+    private final RedisTemplate<Long, Long> redisTemplate;
 
-    public ShowLikeService(ShowService showService, UserService userService, RedisTemplate<Long, Set<Long>> redisTemplate) {
-        this.showService = showService;
-        this.userService = userService;
+    public ShowLikeService(RedisTemplate<Long, Long> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
     @Transactional
     public void createShowLike(User user, Long showId) {
 
-        ShowResponse show = showService.findShowById(showId);
-
-        ValueOperations<Long, Set<Long>> valueOperations = redisTemplate.opsForValue();
-        Set<Long> userIds = valueOperations.get(show.getShowId());
-        if (userIds == null) {
-            userIds = new HashSet<>();
-        }
-        userIds.add(user.getUserId());
-        valueOperations.set(show.getShowId(), userIds);
+        SetOperations<Long, Long> setOperations = redisTemplate.opsForSet();
+        setOperations.add(showId, user.getUserId());
     }
 
     @Transactional
     public void deleteShowLike(User user, Long showId) {
 
-        ValueOperations<Long, Set<Long>> valueOperations = redisTemplate.opsForValue();
-        Set<Long> userIds = valueOperations.get(showId);
-        if (userIds != null) {
-            userIds.remove(user.getUserId());
-            valueOperations.set(showId, userIds);
-        }
+        SetOperations<Long, Long> setOperations = redisTemplate.opsForSet();
+        setOperations.remove(showId, user.getUserId());
     }
 
     @Transactional(readOnly = true)
     public int countShowLikesByShowId(Long showId) {
 
-        Set<Long> userIds = redisTemplate.opsForValue().get(showId);
+        Long count = redisTemplate.opsForSet().size(showId);
 
-        return userIds == null ? 0 : userIds.size();
+        return count == null ? 0 : count.intValue();
     }
 
     @Transactional(readOnly = true)
     public Boolean isShowLikeUser(User user, Long showId) {
 
-        Set<Long> userIds = redisTemplate.opsForValue().get(showId);
+        Set<Long> userIds = redisTemplate.opsForSet().members(showId);
 
         return user != null && userIds != null && userIds.contains(user.getUserId());
     }
