8a194bfd1b3ec3e52e877f1a574475e0b448821c
Joona Choi <juna052@naver.com>
2024-08-04 01:26:15 +0900

    refactor: 공연명 키워드 검색 시 fulltext search 사용하도록 수정

--- culture-ticketing-core/src/main/java/com/culture/ticketing/show/domain/ShowFilter.java
+++ culture-ticketing-core/src/main/java/com/culture/ticketing/show/domain/ShowFilter.java
@@ -1,6 +1,9 @@
 package com.culture.ticketing.show.domain;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberTemplate;
 
 import static com.culture.ticketing.show.domain.QShow.show;
 
@@ -22,9 +25,17 @@ public class ShowFilter {
             builder.and(show.category.eq(this.category));
         }
         if (this.showName != null && !this.showName.isEmpty()) {
-            builder.and(show.showName.contains(showName));
+            builder.and(searchKeyword(showName));
         }
 
         return builder;
+    }
+
+    private BooleanExpression searchKeyword(String keyword) {
+
+        NumberTemplate<Double> booleanTemplate = Expressions.numberTemplate(Double.class,
+                "function('match',{0},{1})", show.showName, keyword);
+
+        return booleanTemplate.gt(0);
     }
 }
